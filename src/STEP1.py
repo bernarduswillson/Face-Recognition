07bcac5a9f8b1c0ef19f356e925338a2f47821b0
Raditss <raditya.naufal.a@gmail.com>
2022-11-03 23:36:16 +0700

    FIX STEP 1

--- src/STEP1.py
+++ src/STEP1.py
@@ -2,7 +2,7 @@ import cv2
 import matplotlib.image as image
 
 def ImgToMtrx(img):
-    image = cv2.imread(r'img')
+    image = cv2.imread(r"src/" + img)
     image = cv2.resize(image,(4,4), interpolation = cv2.INTER_AREA)
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     result = gray_image.flatten()
--- src/main.py
+++ src/main.py
@@ -2,6 +2,6 @@ import STEP1
 import cv2
 import matplotlib.image as image
 
-x = "D:\Algeo02-21021\src\2side.png"
+x = "2side.png"
 a = STEP1.ImgToMtrx(x)
 print(a)
