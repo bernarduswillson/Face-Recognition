deed73e526e507b3c8c417a2ece55d8fba14e9d9
unknown <16521486@mahasiswa.itb.ac.id>
2022-11-11 12:30:34 +0700

    rapihin

--- src/eigenvalue.py
+++ src/eigenvalue.py
@@ -1,20 +1,31 @@
 import numpy as np
-import qr
 
+def QR_Decomposition(A):
+    n, m = A.shape # get the shape of A
 
-def diag_sign(matrix) :
-    D = np.diag(np.sign(np.diag(matrix)))
-    return D
+    Q = np.empty((n, n)) # initialize matrix Q
+    u = np.empty((n, n)) # initialize matrix u
 
-def adjust_sign(Q,R):
-    D = diag_sign(Q)
-    Q[:, :] = np.dot(Q, D)
-    R[:, :] = np.dot(D, R)
+    u[:, 0] = A[:, 0]
+    Q[:, 0] = u[:, 0] / np.linalg.norm(u[:, 0])
+
+    for i in range(1, n):
+
+        u[:, i] = A[:, i]
+        for j in range(i):
+            u[:, i] -= (A[:, i] @ Q[:, j]) * Q[:, j] # get each u vector
+
+        Q[:, i] = u[:, i] / np.linalg.norm(u[:, i]) # compute each e vetor
+
+    R = np.zeros((n, m))
+    for i in range(n):
+        for j in range(i, m):
+            R[i, j] = A[:, j] @ Q[:, i]
 
     return Q, R
 
 def carieigvals(a) :
     for i in range(100) :
-        q, r = qr.qr_decomp(a)
+        q, r = QR_Decomposition(a)
         a = np.dot(r, q)
     return np.diag(a)   
--- src/main.py
+++ src/main.py
@@ -3,19 +3,14 @@ import eigenvalue
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-import qr
 
 start=time.time()
-a = STEP1.covariance2("tes doang")
-a = [[1,1,0],[1,0,1],[0,0,1]]
+a = STEP1.covariance2("pins_Alexandra Daddario")
+# a = [[1,1,0],[1,0,1],[0,0,1]]
 a = np.array(a)
 #eigenvalues = qr.qr_decomp(a)
 eigenvalues = eigenvalue.carieigvals(a)
 print(eigenvalues)
-print("---------------------------------")
-#eigen = np.linalg.qr(a)
-eigen = np.linalg.eigvals(a)
-print(eigen)
 end=time.time()
 print(end-start)
 #plt.imshow(a, cmap='gray')
